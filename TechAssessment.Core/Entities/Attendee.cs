0b496b8c064990184732c80bb39fc0e5b27f5b9d
Fayyaaz <fayyaaz786@gmail.com>
2026-09-10 10:33:27 +0000

    Entities: Add primary ids

--- TechAssessment.Core/Entities/Attendee.cs
+++ TechAssessment.Core/Entities/Attendee.cs
@@ -2,6 +2,7 @@
 {
     public class Attendee
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public List<CalendarEvent> Events { get; set; } = new List<CalendarEvent>();
--- TechAssessment.Core/Entities/CalendarEvent.cs
+++ TechAssessment.Core/Entities/CalendarEvent.cs
@@ -2,6 +2,7 @@
 {
     public class CalendarEvent
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime StartTime { get; set; }
