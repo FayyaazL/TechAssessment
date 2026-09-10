7ac1594d777f0f0db3bf3d5a2e67b10d5c5b522c
Fayyaaz <fayyaaz786@gmail.com>
2026-09-10 09:39:42 +0000

    Core: Update Attendee and Event Definitions

--- TechAssessment.Core/Entities/Attendee.cs
+++ TechAssessment.Core/Entities/Attendee.cs
@@ -2,5 +2,8 @@
 {
     public class Attendee
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<Event> Events { get; set; } = new List<Event>();
     }
 }
--- TechAssessment.Core/Entities/Event.cs
+++ TechAssessment.Core/Entities/Event.cs
@@ -2,5 +2,10 @@
 {
     public class Event
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public List<Attendee> Attendees { get; set; } = new List<Attendee>();
     }
 }
