02a0a39dd41ef7c903834597751ba5a9efe596ea
Fayyaaz <fayyaaz786@gmail.com>
2026-09-10 09:42:32 +0000

    Repositories: Add IEntityRepository.cs and inheriting classes

---
+++ TechAssessment.Repositories/AttendeeRepository.cs
@@ -0,0 +1,9 @@
+﻿using TechAssessment.Core.Entities;
+using TechAssessment.Repositories.Interfaces;
+
+namespace TechAssessment.Repositories
+{
+    public class AttendeeRepository : IEntityRepository<Attendee>
+    {
+    }
+}
---
+++ TechAssessment.Repositories/EventRepository.cs
@@ -0,0 +1,9 @@
+﻿using TechAssessment.Core.Entities;
+using TechAssessment.Repositories.Interfaces;
+
+namespace TechAssessment.Repositories
+{
+    public class EventRepository : IEntityRepository<Event>
+    {
+    }
+}
