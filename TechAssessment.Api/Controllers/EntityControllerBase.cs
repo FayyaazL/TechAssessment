7d5b99376c7c284ff8d9fa2625a811f5d53e9440
Fayyaaz <fayyaaz786@gmail.com>
2026-09-10 09:18:08 +0000

    EntityControllerBase: Add implementation

--- TechAssessment.Api/Controllers/EntityControllerBase.cs
+++ TechAssessment.Api/Controllers/EntityControllerBase.cs
@@ -6,35 +6,44 @@ namespace TechAssessment.Api.Controllers
 {
     public class EntityControllerBase<T> : ControllerBase
     {
-        protected readonly IEnitityService<T> _eventService;
+        protected readonly IEnitityService<T> _entityService;
 
         public EntityControllerBase(IEnitityService<T> eventService)
         {
-            _eventService = eventService;
+            _entityService = eventService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _entityService.GetAllAsync();
         }
 
         [HttpGet("Get")]
         public async Task<T> Get(int id)
         {
-            throw new NotImplementedException();
+            return await _entityService.GetByIdAsync(id);
         }
 
         [HttpPost("Create")]
         public async Task<IActionResult> Create([FromBody] T entity)
         {
-            throw new NotImplementedException();
+            await _entityService.CreateAsync(entity);
+            return Ok();
         }
 
         [HttpPut("Update")]
         public async Task<IActionResult> Update(int id, [FromBody] T entity)
         {
-            throw new NotImplementedException();
+            await _entityService.UpdateAsync(id, entity);
+            return Ok();
         }
 
         [HttpDelete("Delete")]
         public async Task<IActionResult> Delete(int id)
         {
-            throw new NotImplementedException();
+            await _entityService.DeleteAsync(id);
+            return Ok();
         }
     }
 }
